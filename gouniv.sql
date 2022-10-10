edb67cc1a4a2b047b23d2cedeef01442e2d40af4
Doddy MATABARO <matabarododdy@gmail.com>
2022-10-10 12:39:43 +0200

    pdf commands documentations

--- gouniv.sql
+++ gouniv.sql
@@ -1,48 +1,56 @@
-CREATE DATABASE gouniv ;
-
+DROP DATABASE IF EXISTS gouniv;
+CREATE DATABASE gouniv;
+USE gouniv;
 -- create users
-CREATE TABLE staff, staff_no SERIAL PRIMARY KEY, name VARCHAR ;
-CREATE USER Assistant ;
-CREATE USER Admin ;
-CREATE USER Doyer ;
-
+CREATE USER Assistant;
+CREATE USER Admin;
+CREATE USER Doyer;
 -- create admin user
-ALTER USER Admin PASSWORD 'admin' ;
+ALTER USER Admin PASSWORD 'admin';
 ALTER DATABASE gouniv OWNER TO Admin;
-
 -- create tables 
 CREATE TABLE staff(
     staffNO SERIAL PRIMARY KEY,
     name VARCHAR(50),
     region VARCHAR(30)
-);CREATE TABLE student(
+);
+CREATE TABLE student(
     studentId SERIAL PRIMARY KEY,
     name VARCHAR,
     registerred boolean,
     region VARCHAR,
-    staffNO INT REFERENCES staff( staffNO)
-);CREATE TABLE course(
+    staffNO INT REFERENCES staff(staffNO)
+);
+CREATE TABLE course(
     courseCode SERIAL PRIMARY KEY,
     title VARCHAR,
     credit INT,
     quota INT,
     staffNO INT REFERENCES staff(staffNO)
-);CREATE TABLE enrollement(
+);
+CREATE TABLE enrollement(
     studentID INT REFERENCES student(studentID),
     courseCode INT REFERENCES course(courseCode),
     dateEnrolled DATE,
     finalGrade VARCHAR,
-    PRIMARY KEY(studentID,courseCode)
-);CREATE TABLE assignment(
+    PRIMARY KEY(studentID, courseCode)
+);
+CREATE TABLE assignment(
     assignmentNO INT,
     studentID INT REFERENCES student(studentID),
     courseCode INT REFERENCES course(courseCode),
     grade INT,
     PRIMARY KEY(assignmentNO, studentID, courseCode)
 );
-
-
 -- inseriton des donnees 
-INSERT INTO course(title,credit,quota,staffno) VALUES ('Math',4,6,1),('Géo',5,9,10),('Histoire',2,1,5),('Français',2,3,2);
-INSERT INTO staff(name, region) VALUES ('Urbain','Goma'),('Dany','Bukavu'), ('Gloire','Goma'),('Annette','Sankuru'), ('Esther', 'Kindu');
-
+INSERT INTO course(title, credit, quota, staffno)
+VALUES ('Math', 4, 6, 1),
+('Géo', 5, 9, 10),
+('Histoire', 2, 1, 5),
+('Français', 2, 3, 2);
+INSERT INTO staff(name, region)
+VALUES ('Urbain', 'Goma'),
+('Dany', 'Bukavu'),
+    ('Gloire', 'Goma'),
+('Annette', 'Sankuru'),
+    ('Esther', 'Kindu');
